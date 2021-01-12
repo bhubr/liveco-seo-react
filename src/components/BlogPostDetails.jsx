9ee4e1ec106c56df6ba3bbe0ccadf41a2e988311
Benoit Hubert <benoithubert@gmail.com>
2021-01-12 13:18:43 +0100

    Remove unused Link

--- src/components/BlogPostDetails.jsx
+++ src/components/BlogPostDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import db from '../db.json';
 
 function BlogPostDetails() {
   // on ferait un appel API vers le backend pour récupérer un article via son slug
   const { slug } = useParams();
   // récupérer le bon post dans le tableau
-  const post = db.posts.find(p => p.slug === slug);
+  const post = db.posts.find((p) => p.slug === slug);
   return (
     <article className="BlogPostDetails">
       <h2>{post.title}</h2>
